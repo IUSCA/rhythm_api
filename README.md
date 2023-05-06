c4f2caf280868959f64742ac851d0e51f6696cea
Deepak Duggirala <ddeepak6992@gmail.com>
2023-05-05 20:06:18 -0400

    add project and description to workflow, add filter by project,  add swagger ui and doc

--- README.md
+++ README.md
@@ -21,29 +21,9 @@ gunicorn -p app.pid --bind :5001 --workers 1 --threads 1 --timeout 0 scaworkers.
 python -m scaworkers.app
 ```
 
-
+### Test
 ```bash
-[2023-03-24 02:36:58,765: ERROR/MainProcess] Received unregistered task of type 'test.tasks.task1'.
-The message has been ignored and discarded.
-
-Did you remember to import the module containing this task?
-Or maybe you're using relative imports?
-
-Please see
-http://docs.celeryq.org/en/latest/internals/protocol.html
-for more information.
-
-The full contents of the message body was:
-'[["test_id"], {"workflow_id": "870d508b-82b0-45e4-8da3-22c47f42b85b", "step": "step-1"}, {"callbacks": null, "errbacks": null, "chain": null, "chord": null}]' (157b)
-
-Thw full contents of the message headers:
-{'lang': 'py', 'task': 'test.tasks.task1', 'id': 'ee054958-fe5d-46f7-9324-88c4696ca42f', 'shadow': None, 'eta': None, 'expires': None, 'group': None, 'group_index': None, 'retries': 0, 'timelimit': [None, None], 'root_id': 'ee054958-fe5d-46f7-9324-88c4696ca42f', 'parent_id': None, 'argsrepr': "('test_id',)", 'kwargsrepr': "{'workflow_id': '870d508b-82b0-45e4-8da3-22c47f42b85b', 'step': 'step-1'}", 'origin': 'gen26183@Deepaks-MacBook-Air.local', 'ignore_result': False}
-
-The delivery info for this task is:
-{'consumer_tag': 'None4', 'delivery_tag': 1, 'redelivered': False, 'exchange': '', 'routing_key': 'celery'}
-Traceback (most recent call last):
-  File "/Users/deepakduggirala/miniforge3/envs/scaworkers/lib/python3.11/site-packages/celery/worker/consumer/consumer.py", line 591, in on_task_received
-    strategy = strategies[type_]
-               ~~~~~~~~~~^^^^^^^
-KeyError: 'test.tasks.task1'
+python -m celery -A tests.tasks worker --concurrency 2
 ```
+
+This will start celery workers to run tasks in tests.tasks
--- rhythm_api/app.py
+++ rhythm_api/app.py
@@ -1,11 +1,12 @@
+import os
 from datetime import datetime, date
 
-from flask import Flask, request, jsonify
-from flask.json.provider import DefaultJSONProvider
 from celery import Celery
-
-from sca_rhythm import Workflow
+from flask import Flask, request, jsonify, send_file
+from flask.json.provider import DefaultJSONProvider
+from flask_swagger_ui import get_swaggerui_blueprint
 from rhythm_api import celeryconfig
+from sca_rhythm import Workflow, WFNotFound
 
 
 # jsonify - serialize datetime objects into yyyy-mm-ddTHH:mm:ssssss
@@ -22,6 +23,28 @@ celery_app.config_from_object(celeryconfig)
 
 app = Flask(__name__)
 app.json = UpdatedJSONProvider(app)
+
+
+@app.route('/api_docs/swagger.json')
+def swagger_json():
+    # Read before use: http://flask.pocoo.org/docs/0.12/api/#flask.send_file
+    return send_file(os.path.join(app.root_path, '..', 'api_docs', 'swagger.json'))
+
+
+# Define the Swagger UI blueprint
+SWAGGER_URL = '/doc'
+API_URL = '/api_docs/swagger.json'
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Rhythm API"
+    }
+)
+
+# Register the Swagger UI blueprint
+app.register_blueprint(swagger_ui_blueprint)
+
 db = celery_app.backend.database
 wf_col = db.get_collection('workflow_meta')
 task_col = db.get_collection('celery_taskmeta')
@@ -31,11 +54,17 @@ def get_boolean_query(req, name, default=False):
     return req.args.get(name, default, type=lambda v: v.lower() == 'true')
 
 
+def omit_none(d):
+    return {k: v for k, v in d.items() if v is not None}
+
+
 @app.route('/workflow', methods=['GET'])
 def get_workflows():
-    last_task_run = get_boolean_query(request, 'last_task_run')
+    last_task_run = get_boolean_query(request, 'last_task_run', default=True)
     prev_task_runs = get_boolean_query(request, 'prev_task_runs')
     only_active = get_boolean_query(request, 'only_active')
+    project = request.args.get('project')
+
     if only_active:
         active_tasks = task_col.find({
             'status': {
@@ -44,14 +73,30 @@ def get_workflows():
         })
         workflow_ids = [wf_id for t in active_tasks if
                         (wf_id := t.get('kwargs', {}).get('workflow_id', None))]
+        # filter by project
+        result = wf_col.find({
+            'project': project,
+            '_id': {
+                '$in': workflow_ids
+            }
+        }, projection=['_id'])
+        workflow_ids = [res['_id'] for res in result]
     else:
-        workflow_ids = [res['_id'] for res in wf_col.find(projection=['_id'])]
+        query = omit_none({
+            'project': project
+        })
+        workflow_ids = [res['_id'] for res in wf_col.find(query, projection=['_id'])]
     response = []
     for workflow_id in workflow_ids:
         try:
             wf = Workflow(celery_app=celery_app, workflow_id=workflow_id)
             if wf is not None:
-                response.append(wf.get_embellished_workflow(last_task_run=last_task_run, prev_task_runs=prev_task_runs))
+                response.append(
+                    wf.get_embellished_workflow(
+                        last_task_run=last_task_run,
+                        prev_task_runs=prev_task_runs
+                    )
+                )
         except Exception as e:
             print(e)
 
@@ -62,21 +107,34 @@ def get_workflows():
 def get_workflow(workflow_id):
     last_task_run = get_boolean_query(request, 'last_task_run')
     prev_task_runs = get_boolean_query(request, 'prev_task_runs')
-    # TODO: send 404 if there is no workflow with requested workflow_id
-    wf = Workflow(celery_app=celery_app, workflow_id=workflow_id)
-    return jsonify(wf.get_embellished_workflow(last_task_run=last_task_run, prev_task_runs=prev_task_runs))
+
+    try:
+        wf = Workflow(celery_app=celery_app, workflow_id=workflow_id)
+        return jsonify(wf.get_embellished_workflow(last_task_run=last_task_run, prev_task_runs=prev_task_runs))
+    except WFNotFound as exc:
+        return str(exc), 404
 
 
 @app.route('/workflow', methods=['POST'])
 def create_workflow():
-    body = request.json
-    if 'steps' not in body:
-        return "invalid request body", 400
-    if not ('args' in body and isinstance(body['args'], list) and len(body['args']) > 0):
-        return "invalid request body", 400
-    wf = Workflow(celery_app=celery_app, steps=body['steps'], name=body.get('name', None))
-    wf.start(*body['args'])
-    return jsonify({'workflow_id': wf.workflow['_id']})
+    try:
+        body = request.json
+        assert body.get('steps', None), 'missing / invalid "steps" in req body'
+        assert body.get('name', None), 'missing / invalid "name" in req body'
+        assert body.get('project', None), 'missing / invalid "project" in req body'
+        assert isinstance(body.get('args', None), list) and len(body['args']) > 0, 'missing / invalid "args" in req ' \
+                                                                                   'body'
+
+        wf = Workflow(celery_app=celery_app,
+                      steps=body['steps'],
+                      name=body.get['name'],
+                      project=body['project'],
+                      description=body.get('description', None)
+                      )
+        wf.start(*body['args'])
+        return jsonify({'workflow_id': wf.workflow['_id']})
+    except AssertionError as exc:
+        return str(exc), 400
 
 
 @app.route('/workflow/<workflow_id>/pause', methods=['POST'])
