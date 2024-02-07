43080668892ff931e201a952973e9ee28b9f5577
Deepak Duggirala <ddeepak6992@gmail.com>
2024-02-07 14:37:50 -0500

    Fix docker-compose command in deploy.sh and docker-compose files

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,9 +4,9 @@ if [ -z "$RELEASE" ]; then
     echo "No release specified - defaulting to production" 
     echo "Production mode"
     sudo docker compose -f "docker-compose-prod.yml" build api
-    sudo docker compose -f "docker-compose-prod.yml" up -d
+    sudo docker compose -f "docker-compose-prod.yml" up -d --force-recreate api
 else
     echo "Dev mode"
     sudo docker compose -f "docker-compose-dev.yml" build api
-    sudo docker compose -f "docker-compose-dev.yml" up -d
+    sudo docker compose -f "docker-compose-dev.yml" up -d --force-recreate api
 fi
